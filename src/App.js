a788b56b5dc18e7f9647ca60d862484140c2854b
syed jasmine <smsaf91@gmail.com>
2024-12-12 13:00:01 +0530

    message

--- src/App.js
+++ src/App.js
@@ -135,7 +135,7 @@ function App() {
         
           <Route path="/:engalaipatri" element={<Engalaipatri/>} />
           <Route path="/Collections/:Collections" element={<Thoguppugal/>} />
-          <Route path="/*/:நிறுவனர்" element={<Engalaipatricategory/>} />
+          <Route path="/*/:niruv" element={<Engalaipatricategory/>} />
 
           <Route path="/nigazhchigal/:nigazhchigal" element={<Nigazhchigal/>} />
           <Route path="/Vahdhathulujjuth/:Vahdhathulujjuth" element={<Vahthathul1/>} />
