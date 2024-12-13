a7598e8ec69528858666a67413868801b33b42b3
syed jasmine <smsaf91@gmail.com>
2024-12-13 15:43:29 +0530

    message

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-//baseURL: 'https://emsmedia.net:4014',
-baseURL: 'http://localhost:5009'
+baseURL: 'https://emsmedia.net:4014',
+//baseURL: 'http://localhost:5009'
 });
 
 export default api
